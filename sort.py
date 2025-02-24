70364483907fee32a15b75fd141fa68fb6153b28
Richard Naik <richardnaik@gmail.com>
2025-02-23 20:28:46 -0600

    config file

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
 import os
-import shutil
+from shutil import copyfile
+import configparser
 from pprint import pprint
 from PIL import Image
 from pillow_heif import register_heif_opener
@@ -9,12 +10,15 @@ import ffmpeg
 # need to call to be able to recognize HEIC files
 register_heif_opener()
 
+config = configparser.RawConfigParser() 
+config.read_file(open(r"./config.cfg")) 
+
 # directory with unsorted media
-unsorted_dirname = "/home/rick/code/unsorted-media"
+unsorted_dirname = config.get('directories', 'unsorted')
 unsorted_dir = os.fsencode(unsorted_dirname)
 
 # directory for sorted media
-sorted_dirname = "/home/rick/code/sorted-media"
+sorted_dirname = config.get('directories', 'sorted')
 
 # loop through directory
 for file in os.scandir(unsorted_dir):
@@ -32,10 +36,12 @@ for file in os.scandir(unsorted_dir):
         # TODO handle files with no exif date or no extension
         # get the creation date/time from exif data
         original_create_datetime = exifdata.get(306)
+        new_filename = sorted_dirname + "\\" + original_create_datetime + extension
+        print(new_filename)
 
         # copy file to sorted dir with new name based on exif date
-        print(f"Renaming {full_filename} to {original_create_datetime}{extension}")
-        shutil.copyfile(full_filename, sorted_dirname + '/' + original_create_datetime + extension)
+        print(f"Renaming {full_filename} to {new_filename}")
+        copyfile(full_filename, new_filename)
 
     # handle videos
     elif filename.lower().endswith(".mp4") or filename.lower().endswith(".mov"):
